72386cb04068942e7dedcbf33d805d4b65aedae3
GuštinGal <gal.gustin@gmail.com>
2026-02-12 13:45:22 +0100

    supabase keys

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,8 +3,8 @@ import { Database } from './database.types';
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://coomqgvkdkbpttcotpki.supabase.co';
-const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvb21xZ3ZrZGticHR0Y290cGtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1NzQwODcsImV4cCI6MjA4NjE1MDA4N30.3SD6OrarY4iN5AysVH9x0FejfxTRFzEqDtXOjSPXlHE';
+const SUPABASE_URL = 'https://coomqgvkdkbpttcotpki.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvb21xZ3ZrZGticHR0Y290cGtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1NzQwODcsImV4cCI6MjA4NjE1MDA4N30.3SD6OrarY4iN5AysVH9x0FejfxTRFzEqDtXOjSPXlHE';
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
     auth: {
